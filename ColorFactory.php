515c0d0f6a3a56df9c240a137a6e9307713274c0
Abbas Shakiba <shakiba_9@yahoo.com>
2017-05-21 05:42:15 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- ColorFactory.php
+++ ColorFactory.php
@@ -1,28 +1,29 @@
 <?php
+
 namespace app\components\color;
 
 /**
- * Class ColorFactory
- * @package app\components\color
+ * Class ColorFactory.
  */
 class ColorFactory
 {
     private static $message;
     private static $_instance;
 
-
     public static function text($text)
     {
         if (self::$_instance === null) {
-            self::$_instance = new self;
+            self::$_instance = new self();
         }
         self::$message = $text;
+
         return self::$_instance;
     }
 
     public function color($color)
     {
         $c = new Color();
+
         return $c(self::$message)->highlight($color);
     }
 }
